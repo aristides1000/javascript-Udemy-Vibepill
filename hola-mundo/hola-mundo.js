6282d0c4aef6b2a18463552686339a5bfc5a9672
aristides1000 <aristides1000@gmail.com>
2019-07-30 09:54:33 -0400

    18. Variables y comentarios

---
+++ hola-mundo/hola-mundo.js
@@ -0,0 +1,10 @@
+alert("Hola Mundo con JS");
+alert("Bienvenido al curso");
+
+/* si yo quiero escribir algo desde este archivo en la pàgina web, lo puedo hacer de la siguiente manera */
+
+document.write("hola mundo desde archivo externo javascript");
+
+/* si queremos mostrar algo en la consola debemos hacer lo siguiente */
+console.log("muestra esto en la consola");
+console.log(84+6);/* en javascript tambien se pueden hacer operaciones matemáticas haciendo uso del console.log() como se muestra en este ejemplo, pero no es una buena práctica y puede que genere algunos errores */
